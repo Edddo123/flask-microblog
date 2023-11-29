fafa28806ce411bc4e6d47aedab9cb08fda41428
Edddo123 <edo.miraqian@gmail.com>
2023-11-29 20:20:34 +0400

    added login/logout - send production error notifications to email

--- app/models.py
+++ app/models.py
@@ -1,16 +1,22 @@
 from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
+
+# creates default methods for user model required by flask login
+from flask_login import UserMixin
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     # backref will make post.author. This will not be field in db, only user_id in Post will be. It is just on application level for ORM
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
@@ -23,6 +29,11 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
     
 #     >>> from app.models import User
 # >>> u = User(username='susan', email='susan@example.com')
